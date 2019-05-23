9db9386d7f06e88a893ec417fb402dc52f66974b
Michael Holt <cutflame@gmail.com>
2019-05-23 15:25:28 -0600

    fix package manager settings

--- Package.swift
+++ Package.swift
@@ -1,19 +1,20 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
-    name: "MeasurementFormatter.com",
+    name: "MeasurementFormatter",
+    platforms: [ .macOS(.v10_12) ],
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
 
         // 🍃 An expressive, performant, and extensible templating language built for Swift.
-        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0")
     ],
     targets: [
-        .target(name: "App", dependencies: ["Leaf", "Vapor"]),
-        .target(name: "Run", dependencies: ["App"]),
+        .target(name: "App", dependencies: ["Leaf", "Vapor"], exclude: ["derivedData"]),
+        .target(name: "Run", dependencies: ["App"], exclude: ["derivedData"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
-    ]
+    ],
+    swiftLanguageVersions: [ .v5 ]
 )
-
